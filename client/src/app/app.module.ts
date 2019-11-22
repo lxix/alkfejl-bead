79be9df21fa2246d42b6f13dbd678f98fd9af24d
Ládi Péter <ladi.peter@csbo.hu>
2019-11-22 14:15:11 +0100

    Add home page plus basic menubar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './routing/routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,14 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    NgbModule,
     RoutingModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
